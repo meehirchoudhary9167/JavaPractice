ce9c5f1d3dd8a57949279f395b4517a6c6d063af
meehir <meehirchoudhary86@gmail.com>
2025-07-22 14:25:22 +0530

    "Added Day 2 Java programs and organized code in my package"

--- src/Day2/SortArray.java
+++ src/Day2/SortArray.java
@@ -1,5 +1,13 @@
 package Day2;
 
-public class SortArray {
+import java.util.Arrays;
 
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1};
+        Arrays.sort(arr);
+
+        System.out.print("Sorted Array: ");
+        for (int val : arr) System.out.print(val + " ");
+    }
 }
