70c0e3f816d3233d1a322ca1f29c4c252d585a4b
meehir <meehirchoudhary86@gmail.com>
2025-07-22 16:10:19 +0530

    OOPS TASK part 1

---
+++ src/OOPS/StudentTest.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Ayush_Wipro";
+        s1.age = 18;
+        s1.display();
+    }
+}
